ea314acf1d2ba1ef039bbb282dca59818666ae8a
Peng <pzfight520@163.com>
2020-07-29 22:54:19 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Tabs, TabPane, Alert, Cascader, Option, Select, Tree, Tag, MessageBox, Dialog, Pagination, Tooltip, Switch, Table, TableColumn, Row, Col, Card, BreadcrumbItem, Breadcrumb, Form, FormItem, Input, Message, Button, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
+import { Upload, CheckboxGroup, Checkbox, Steps, Step, Tabs, TabPane, Alert, Cascader, Option, Select, Tree, Tag, MessageBox, Dialog, Pagination, Tooltip, Switch, Table, TableColumn, Row, Col, Card, BreadcrumbItem, Breadcrumb, Form, FormItem, Input, Message, Button, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
 
 //Vue.use()注册为全局组件
 Vue.use(Button)
@@ -33,5 +33,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 Vue.prototype.$message = Message //弹框组件通过this可以访问
 Vue.prototype.$confirm = MessageBox.confirm
